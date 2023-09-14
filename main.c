9ab6b633cae93bd9dcb4f4e4bc27a34a68131b53
jonique98 <tnals9899@naver.com>
2023-09-14 23:07:13 +0900

    hi

--- main.c
+++ main.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
 #include <pthread.h>
+ #include <sys/time.h>
 
 pthread_mutex_t mutex;
 
 void *hi(void *a) {
+    struct timeval time;
     a = 0;
 
+while (1)
+{
     pthread_mutex_lock(&mutex);
 
-
-    printf("hi\n");
-
+    gettimeofday(&time, NULL);
     pthread_mutex_unlock(&mutex);
-    pthread_mutex_unlock(&mutex);
+}
+
 
     return a;
 }
 
 int main() {
-    pthread_t thread;
 
-    pthread_mutex_init(&mutex, NULL);
+    pthread_t thread[10000000];
+    int i = 0;
 
-    pthread_create(&thread, NULL, hi, NULL);
-    pthread_join(thread, NULL);
+    while (i < 10000000)
+    {
+        pthread_mutex_init(&thread[i], NULL);
+        pthread_create(&thread[i], NULL, hi, NULL);
+        i++;
+    }
+    i = -1;
+    while (++i < 10000000)
+        pthread_join(thread[i], NULL);
 
     pthread_mutex_destroy(&mutex);
 
